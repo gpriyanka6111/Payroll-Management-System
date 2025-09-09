730bd7819ca3597e0fa4088174885947e6c12fb9
Priyanka Gulgari <priyankagulgari87@gmail.com>
2025-09-09 19:33:17 +0000

    There is gap, The places where I have drawn.I dont want that that speace

--- src/app/(app)/dashboard/manager/layout.tsx
+++ src/app/(app)/dashboard/manager/layout.tsx
@@ -26,14 +26,14 @@ export default function ManagerLayout({
   ];
 
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+    <div className="grid w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
-        <div className="flex h-full max-h-screen flex-col gap-2">
+        <div className="flex h-full max-h-screen flex-col">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
              <h2 className="text-lg font-semibold">Manager Area</h2>
           </div>
           <div className="flex-1">
-            <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+            <nav className="grid items-start px-2 py-4 text-sm font-medium lg:px-4">
               {navLinks.map(link => (
                 <Link
                   key={link.href}
