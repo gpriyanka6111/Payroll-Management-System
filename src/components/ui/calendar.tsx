a5950d6d738bb1cb74903247cf689695ac3132bc
gpriyanka6111 <>
2025-12-11 20:08:06 +0000

    still same error

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -55,8 +55,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
-        IconRight: () => <ChevronRight className="h-4 w-4" />,
+        ChevronLeft: () => <ChevronLeft className="h-4 w-4" />,
+        ChevronRight: () => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
